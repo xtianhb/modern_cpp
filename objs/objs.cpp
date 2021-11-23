9a18fc0696528544b58995e32e72373f6e092893
Xtian.Belussi <cristian.belussi@gmail.com>
2021-11-23 11:32:12 -0300

    memory

--- objs/objs.cpp
+++ objs/objs.cpp
@@ -2,8 +2,193 @@
 #include <objs.hpp>
 #include <iostream>
 
+/* 
+OOO REVIEW
+
+Abstraction:  The main purpose of abstraction is hiding the unnecessary 
+details from the users. Hides the internal implementation details.
+The objects have interfaces that abstratc the inner working.
+
+Encapsulation: Refers to the bundling of data, along with the methods that 
+operate on that data, into a single unit, and restricting the access 
+to some of an object's components.
+
+Inheritance: is the mechanism of basing an object or class upon another 
+object (prototype-based inheritance) or class (class-based inheritance), 
+retaining similar implementation. Promotes reuse and extensebility.
+
+Composition: Signifies relation between objects. It has some characteristic.
+
+Polymorphism: Common behaviour with different implementations. Different
+objects can have similar behaviour with a different impl. 
+Overloading, virtual functions, templates.
+Promotes reuse, scalability, extensibility.
+
+*/
+
+
 void TestObjs(void)
 {
     std::cout << "TEST OBJS" << std::endl;
 
+    const cCar * Car; 
+    std::cout << "A" << std::endl;
+    Car = new const cCar(); // Default constructor
+    // Const qualifier, qualifies *this* pointer. Object, members, constant
+    std::cout << "B" << std::endl;
+    cCar().PrintCars();
+    std::cout << "C" << std::endl;
+    Car->PrintCars();
+    std::cout << "D" << std::endl;
+    Car->DashBoard();
+
+    cCar * nCar;
+    nCar = new cCar(123, 456.7); //Default constructor
+    nCar->Acc();
+    nCar->DashBoard();
+    delete nCar;
+
+    delete Car;
+
+    std::cout << "------------" << std::endl;
+
+    // Structs
+    Point Start, End;
+    Start.x=1; Start.y=2;    
+    DrawLine(Start, End);
+
+    std::cout << "------------" << std::endl;
+
+    // Copy constructor
+    int n = 1;
+    int * p1 = &n;
+    int *p2 = p1; //Shallow copy
+    int *p3 = new int(*p1); //Deep copy
+
+    Integer Val1(5);
+    Integer Val2(Val1);
+    std::cout << Val1.GetVal() << std::endl;
+
+    // If there is one or more of 
+    //      Destructor, Copy constructor, or Copy assignment
+    // then the three should be defined.
+    // Add deep copy
+
+    std::cout << "------------" << std::endl;
+
+    Int A;
+    Int B(3);
+    //Int C(A); Not possible. Deleted default copy ctor.
+    //A.SetValue(3.0); Not possible. Deleted 
+
+    return;
+}
+
+Integer::Integer(const Integer & Obj)
+{
+    p = new int( *Obj.p);
+}
+Integer::~Integer()
+{
+    delete p;
+}
+Integer::Integer()
+{
+    std::cout << "Integer()" << std::endl;
+    p = new int(0);
+}
+Integer::Integer(int v)
+{
+    std::cout << "Integer(v)" << std::endl;
+    p = new int(v);
+}
+int Integer::GetVal() const
+{
+    std::cout << "GetVal()" << std::endl;
+    return *p;
+}
+void Integer::SetVal(int v)
+{
+    std::cout << "SetVal(v)" << std::endl;
+    *p = v;
+}
+
+// Static variable
+int cCar::TotalCars = 0;
+
+void cCar::PrintCars(void)
+{
+    // No *this* pointer
+    std::cout << "   Total=" << TotalCars << std::endl;
+}
+
+void cCar::DashBoard(void) const
+{
+    // Const qualifier, qualifies *this* pointer.
+    // Object, memebers, constant
+    std::cout << "Fuel=" << Fuel; 
+    std::cout << " Passengers=" << Passengers;
+    std::cout << " Speed=" << Speed;
+    std::cout << std::endl;
+}
+
+void Foo(cCar &Car)
+{
+    return;
+}
+
+cCar::~cCar()
+{
+    std::cout << "~cCar"<< std::endl;
+    TotalCars--;
+}
+
+
+cCar::cCar(void):cCar(0)
+{
+    // Third
+    std::cout << "cCar()"<< std::endl;
+}
+
+cCar::cCar(int p ):cCar(p,0.0)
+{
+    // Second
+    std::cout << "cCar(p)"<< std::endl;
+}
+
+cCar::cCar(int p, float f)
+{
+    // First
+    std::cout << "cCar(p,f)"<< std::endl;
+    Foo(*this);
+    TotalCars++;
+
+    Fuel = f;
+    Passengers = p;
+    Speed=0;
+}
+
+void cCar::Acc()
+{
+    //implicit -this- pointer.
+    //this->Speed ++;
+    Speed++;
+    Fuel -=0.01;
+    std::cout << "Acc()" << std::endl;
+}
+
+void cCar::Brake()
+{
+    Speed=0;
+}
+
+void cCar::FillFuel(float f)
+{
+    Fuel += f;
+}
+
+void DrawLine(Point Start, Point End)
+{
+    std::cout << "DrawLine: " ;
+    std::cout << Start.x << ", " << Start.y << std::endl;
 }
--- objs/objs.hpp
+++ objs/objs.hpp
@@ -1,1 +1,84 @@
+#pragma once
+
 void TestObjs(void);
+
+
+class Int 
+{
+private:
+    int Value=0;
+public:
+    // Directive, define default constructor.
+    // Same applies to constc, copy const, assign const.
+    Int() = default;     
+    Int (int V)
+    {
+        Value=V;
+    }
+    // Don't synthesize copy const.
+    Int(const Int &) = delete;
+    void SetValue(int V)
+    {
+        Value = V;
+    }
+    // delete can be used in other functions
+    void SetValue(float) = delete;
+};
+
+
+
+
+class Integer
+{
+private:
+    int *p;
+public:
+    Integer();
+    Integer(int v);
+    Integer(const Integer &); //This is a copy constructor
+    int GetVal()const;
+    void SetVal(int v);
+    ~Integer();
+};
+
+
+class cCar
+{
+//private: by default
+private:
+    //Non static data member initializer
+    float Fuel=5.0;
+    float Speed=0.0;
+    int Passengers=0;
+
+public:
+    static int TotalCars; // Static member
+    static void PrintCars(void); //Static funcion.
+    void FillFuel(float amount);
+    void Acc();
+    void Brake();
+    void DashBoard() const;
+
+    cCar(int p, float f);
+    cCar(int p );
+    cCar(void);
+    ~cCar();
+};
+
+void DrawLine(int , int , int , int );
+struct Point
+{
+//Default access: public    
+    int x;
+    int y;
+};
+void DrawLine(Point Start, Point End);
+
+class SInit
+{
+    //Non static data member initializer
+    float a=1.0;
+    int b=1;
+    //Any other type.
+    //Goes to default constructor.
+};
